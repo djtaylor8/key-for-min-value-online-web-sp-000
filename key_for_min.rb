fce1f6729404468c27bbd7db9d45873a1f07cfa2
DJ Taylor <dtaylor88@gmail.com>
2021-06-01 18:18:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
     min_value = value[index]
     if value[index + 1] < value[index]
       min_value = value[index + 1]
-      min_key = key
+      min_key = key[index + 1]
       index += 1
     end
   end
