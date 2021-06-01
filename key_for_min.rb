95af73fb2e6e39fd9b640fa6462cb15270a828d1
DJ Taylor <dtaylor88@gmail.com>
2021-06-01 18:12:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   index = 0
   name_hash.each do |key, value|
     min_value = value[index]
-    if value[index] < value[index + 1]
-      min_value = value[index]
+    if value[index + 1] < value[index]
+      min_value = value[index + 1]
       min_key = key
       index += 1
     end
